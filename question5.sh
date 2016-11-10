c97836fcc0c3f9a2f3388fee39457972d9591175
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-11-10 18:10:29 -0500

    Update question5.sh

--- question5.sh
+++ question5.sh
@@ -3,8 +3,7 @@
 
 #Luisa Aimoli 10169687
 #Looks for all executable files in the current directory
-#Adds "#Developed in cisc220"after #!/bin/bash
-#Developed in cisc220
+#Adds "#Developed in cisc220" after #!/bin/bash
 
 find . -type f -exec sed --silent '\:#!/bin/bash:a\
 #Developed in cisc220' {} +
