3913a79921dca4bd2ccd543ccf62facda197fa03
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-11-10 23:43:29 -0500

    Create joiningTables.sh

---
+++ joiningTables.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#joins two tables using a relationship table
+#prints out student name and course name
+#sort output by student name, then course name
+
+declare -A courses
+declare -A students
+
+cat course.txt | while read line
+do
+  first=$(echo $line | awk '{print $1}')
+  second=$(echo $line | awk '{print $2}')
+  courses+=( ["$first"]=$second )
+done
+cat student.txt | while read line
+do
+  first=$(echo $line | awk '{print $1}')
+  second=$(echo $line | awk '{print $2}')
+  students+=( ["$first"]=$second )
+done
+
+for i in "$(!courses[@])"
+do
+  grep $i student_course.txt #key
+  $(courses[$i]} | sed #value
+done
+for i in "${!students[@])"
+do
+  grep $i student_course.txt #key
+  $(students[$i]) | sed #value
+done
