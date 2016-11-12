a2f6e28125958d2a6a269e9bcf82e1e7f5a028f9
braedanR <braedantye@msn.com>
2016-11-11 23:31:31 -0500

    Hello

--- question4.sh
+++ question4.sh
@@ -4,4 +4,6 @@
 #Display only the two partitions with largest sizes
 #Sorts only partitions
 
-lsblk --noheadings --sort SIZE -raw | grep 'part' | head -2
+lsblk --noheadings --raw | grep 'part'| sort -k4g
+
+
