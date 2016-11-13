d8296fe565afb3fe1519385ab48be5ced984ece4
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-11-13 18:43:22 -0500

    ??

--- joiningTables.sh
+++ joiningTables.sh
@@ -6,28 +6,35 @@
 
 declare -A courses
 declare -A students
+cp students_courses.txt final.txt
 
-cat course.txt | while read line
+cat courses.txt | while read line #reads the file per line
 do
-  first=$(echo $line | awk '{print $1}')
-  second=$(echo $line | awk '{print $2}')
-  courses+=( ["$first"]=$second )
+  first=$(echo $line | awk '{print $1}') #get courseID
+  second=$(echo $line | awk '{print $2}') #get courseName
+  courses+=( [$first]=$second ) #add to course "dictionary"
 done
-cat student.txt | while read line
+cat students.txt | while read line #reads the file per line
 do
-  first=$(echo $line | awk '{print $1}')
-  second=$(echo $line | awk '{print $2}')
-  students+=( ["$first"]=$second )
-done
-
-for i in "${(!courses[@])}"
-do
-  grep $i student_course.txt #key
-  ${(courses[$i]} | sed #value
-done
-for i in "${!students[@])}"
-do
-	grep $i | student.course.txt #key
-	${(students[$1]} | sed #value
+  first=$(echo $line | awk '{print $1}') #get studentID
+  second=$(echo $line | awk '{print $2}') #get studentName
+  students+=( [$first]=$second ) #add to student "dictionary"
 done
 
+for i in "${!courses[@]}"
+do
+	if [[ $i != courseID ]] ; then
+		sed "s/$i/${courses[$i]}/" final.txt #replace key (courseID) for value (courseName)
+	fi
+done
+for i in "${!students[@]}"
+do
+	if [[ $i != studentID ]] ; then
+		sed "s/$i/${students[$i]}/" final.txt #replace KEY (studentID) for value (studentName)
+	fi
+done
+
+sed s/studentID/studentName/ final.txt > /dev/null #no output
+sed s/courseID/courseName/ final.txt > /dev/null #no output
+tail -10 final.txt | sort > /dev/null #sorts student names #no output
+tail -10 final.txt | sort -k2 #sorts course names
