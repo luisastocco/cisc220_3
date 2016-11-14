2cd5ab4a837b890a25c44c45d82755af145bba4c
braedanR <braedantye@msn.com>
2016-11-14 10:27:41 -0500

    The file names didn't match the names of the files given in the assignment

--- joiningTables.sh
+++ joiningTables.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
 #Luisa Aimoli 10169687
+#Braedan Robinson 10188414
 #joins two tables using a relationship table
 #prints out student name and course name
 #sort output by student name, then course name
 
-cat students_courses.txt > final.txt #copy file content to final.txt
+cat student_course.txt > final.txt #copy file content to final.txt
 
 while read line #reads the file per line
 do
   first=$(echo $line | awk '{print $1}') #get studentID
   second=$(echo $line | awk '{print $2}') #get studentName
   sed -i "s/$first/$second/" final.txt #replaces match of ID for name
-done < students.txt
+done < student.txt
 
 while read line #reads the file per line
 do
   first=$(echo $line | awk '{print $1}') #get courseID
   second=$(echo $line | awk '{print $2}') #get courseName
   sed -i "s/\<$first\>/$second/" final.txt #replaces exact match of ID for name
-done < courses.txt
+done < course.txt
 
 tail -10 final.txt | sort #sorts student names
 #tail -10 final.txt | sort -k2 #sorts course names #couldn't figure out how to sort it without unsorting the students
