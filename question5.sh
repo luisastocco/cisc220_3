1773bd7cebcf48efd2ac2a8a6908cd82eec641c5
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-11-13 20:04:50 -0500

    Update question5.sh

--- question5.sh
+++ question5.sh
@@ -2,8 +2,8 @@
 #Developed in cisc220
 
 #Luisa Aimoli 10169687
-#Looks for all executable files in the current directory
+#Looks for all executable files in the current directory (. is current dir, f is file, -exec is executable)
 #Adds "#Developed in cisc220" after #!/bin/bash
 
-find . -type f -exec sed --silent '\:#!/bin/bash:a\
-#Developed in cisc220' {} +
+find . -type f -exec sed --silent '\:#!/bin/bash:a\ 
+#Developed in cisc220' {} + #sed command syntax obtained and adapted from multiple stackoverflow links
