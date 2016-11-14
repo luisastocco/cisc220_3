81652391f35c33c6d4ed63e683cd26ccebe263ea
luisastocco <luisa_stoccoaimoli@hotmail.com>
2016-11-13 19:56:27 -0500

    ok

--- joiningTables.sh
+++ joiningTables.sh
@@ -4,37 +4,24 @@
 #prints out student name and course name
 #sort output by student name, then course name
 
-declare -A courses
-declare -A students
-cp students_courses.txt final.txt
+declare -A COURSES
+declare -A STUDENTS
+count=1
+cat students_courses.txt > final.txt
 
-cat courses.txt | while read line #reads the file per line
-do
-  first=$(echo $line | awk '{print $1}') #get courseID
-  second=$(echo $line | awk '{print $2}') #get courseName
-  courses+=( [$first]=$second ) #add to course "dictionary"
-done
-cat students.txt | while read line #reads the file per line
+while read line #reads the file per line
 do
   first=$(echo $line | awk '{print $1}') #get studentID
   second=$(echo $line | awk '{print $2}') #get studentName
-  students+=( [$first]=$second ) #add to student "dictionary"
-done
+  sed -i "s/$first/$second/" final.txt
+done < students.txt
 
-for i in "${!courses[@]}"
+while read line #reads the file per line
 do
-	if [[ $i != courseID ]] ; then
-		sed "s/$i/${courses[$i]}/" final.txt #replace key (courseID) for value (courseName)
-	fi
-done
-for i in "${!students[@]}"
-do
-	if [[ $i != studentID ]] ; then
-		sed "s/$i/${students[$i]}/" final.txt #replace KEY (studentID) for value (studentName)
-	fi
-done
+  first=$(echo $line | awk '{print $1}') #get courseID
+  second=$(echo $line | awk '{print $2}') #get courseName
+  sed -i "s/\<$first\>/$second/" final.txt
+done < courses.txt
 
-sed s/studentID/studentName/ final.txt > /dev/null #no output
-sed s/courseID/courseName/ final.txt > /dev/null #no output
-tail -10 final.txt | sort > /dev/null #sorts student names #no output
-tail -10 final.txt | sort -k2 #sorts course names
+tail -10 final.txt | sort #sorts student names
+#tail -10 final.txt | sort -k2 #sorts course names
